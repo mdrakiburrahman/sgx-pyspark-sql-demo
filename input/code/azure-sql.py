ed81a30545cd0074ba7858baf54d0ac68b6eeb1c
Raki <46581776+mdrakiburrahman@users.noreply.github.com>
2021-04-16 17:39:33 -0400

    Update azure-sql.py

--- input/code/azure-sql.py
+++ input/code/azure-sql.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     jdbcDF = spark.read \
         .format("jdbc") \
         .option("url", "") \
-        .option("table", "SELECT TOP (10) * FROM dbo.Employees") \
+        .option("dbtable", "dbo.Employees") \
         .load()
 
     jdbcDF.show()
