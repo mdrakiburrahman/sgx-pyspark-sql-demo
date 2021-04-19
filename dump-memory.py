6ff47ed32419d2d8432297d8d517b593ee275c6b
Raki Rahman <46581776+mdrakiburrahman@users.noreply.github.com>
2021-04-18 22:09:15 -0400

    Update dump-memory.py

--- dump-memory.py
+++ dump-memory.py
@@ -8,29 +8,22 @@ def print_memory_of_pid(pid, only_writable=True):
     """
     memory_permissions = 'rw' if only_writable else 'r-'
     sys.stderr.write("PID = %d \n" % pid)
-    try:
-        with open("/proc/%d/maps" % pid, 'r') as maps_file:
-            with open("/proc/%d/mem" % pid, 'r', 0) as mem_file:
-                for line in maps_file.readlines():  # for each mapped region
-                    m = re.match(r'([0-9A-Fa-f]+)-([0-9A-Fa-f]+) ([-r][-w])', line)
-                    if m.group(3) == memory_permissions: 
-                        sys.stderr.write("\nOK : \n" + line+"\n")
-                        start = int(m.group(1), 16)
-                        if start > 0xFFFFFFFFFFFF:
-                            continue
-                        end = int(m.group(2), 16)
-                        sys.stderr.write( "start = " + str(start) + "\n")
-                        mem_file.seek(start)  # seek to region start
-                        chunk = mem_file.read(end - start)  # read region contents
-                        print(chunk)  # dump contents to standard output
-                    else:
-                        sys.stderr.write("\nPASS : \n" + line+"\n")
-    except e:
-        print("-------------------")
-        print("Memory scan failed")
-        print("-------------------")
-        raise e
-
+    with open("/proc/%d/maps" % pid, 'r') as maps_file:
+        with open("/proc/%d/mem" % pid, 'r', 0) as mem_file:
+            for line in maps_file.readlines():  # for each mapped region
+                m = re.match(r'([0-9A-Fa-f]+)-([0-9A-Fa-f]+) ([-r][-w])', line)
+                if m.group(3) == memory_permissions: 
+                    sys.stderr.write("\nOK : \n" + line+"\n")
+                    start = int(m.group(1), 16)
+                    if start > 0xFFFFFFFFFFFF:
+                        continue
+                    end = int(m.group(2), 16)
+                    sys.stderr.write( "start = " + str(start) + "\n")
+                    mem_file.seek(start)  # seek to region start
+                    chunk = mem_file.read(end - start)  # read region contents
+                    print(chunk)  # dump contents to standard output
+                else:
+                    sys.stderr.write("\nPASS : \n" + line+"\n")
 
 if __name__ == '__main__': # Execute this code when run from the commandline.
     try:
