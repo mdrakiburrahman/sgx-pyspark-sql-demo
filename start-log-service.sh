7e91c42f173e71a57c447d2086dbfdbd0bd7c94b
Raki Rahman <46581776+mdrakiburrahman@users.noreply.github.com>
2021-04-19 21:57:24 -0400

    Update start-log-service.sh

--- start-log-service.sh
+++ start-log-service.sh
@@ -1,4 +1,8 @@
-pkill -9 node
-nohup log.io-startup &> /var/log/log.io.txt &
-nohup log.io-file-input &> /var/log/log.io.txt & 
-nohup log.io-server &> /var/log/log.io.txt &
+#pkill -9 node
+
+# Old
+#nohup log.io-startup &> /var/log/log.io.txt &
+
+# New
+#nohup log.io-file-input &> /var/log/log.io.txt & 
+#nohup log.io-server &> /var/log/log.io.txt &
